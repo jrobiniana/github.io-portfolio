86b1d87f419f96c5bc364070899dbe85e8ebf4a9
jrobiniana <jrobiniana@gmail.com>
2026-05-29 11:37:38 +0800

    Added .lock file.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  output: 'export'
+  output: 'export',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
